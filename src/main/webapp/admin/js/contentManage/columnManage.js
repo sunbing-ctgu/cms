39df66265d0b0ad5fe96e10d5fd6be943a5fa757
sunbing-ctgu <545130993@qq.com>
2018-01-21 00:05:40 +0800

    1.添加查询接口

--- src/main/webapp/admin/js/contentManage/columnManage.js
+++ src/main/webapp/admin/js/contentManage/columnManage.js
@@ -2,6 +2,46 @@
  * 
  */
 let treeCache;
+
+class QueryParam {
+	constructor() {
+		this.columnId;
+		this.columnName;
+		this.pageSize = 10;
+		this.pageNum = 1;
+	}
+}
+class Column {
+	constructor() {
+		this.name;
+		this.rename;
+		this.channel;
+		this.sort;
+		this.createTime;
+	}
+}
+
+let param = new QueryParam();
+
+let column = Column();
+
+class QueryColumn {
+	static query(data) {
+		return new Promise((resolve) => {
+			$.ajax({
+				url: '/admin/columnManage/getColumnList',
+				type: 'POST',
+				async: true,
+				contentType: "application/json; charset=utf-8",
+	            data: JSON.stringify(data),
+	            dataType: 'json'
+			}).then((result) => {
+				resolve(result)
+			});
+		});
+	}
+}
+
 class QueryTree {
 	static getColumnTree() {
 		return new Promise((resolve) => {
@@ -23,7 +63,7 @@ $(function(){
 		$('#tree').treeview({
 	   		data: treeCache,
 	   		onNodeSelected: function(event, node) {
-	   			console.log(node.text + 'was selected');
+	   			console.log("id:" + node.id + "text:" + node.text + 'was selected');
 	   		}
 	   	});
 	});
